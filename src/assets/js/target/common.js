c181de53da49d6dce82409b459f78f3bb1bc67d3
hironori niho <hironori103@gmail.com>
2021-01-20 12:02:10 +0900

    ・グローバルナビの追加

--- src/assets/js/target/common.js
+++ src/assets/js/target/common.js
@@ -1,3 +1,15 @@
 {
-    console.log('common.js');
+    const globalMenu = () => {
+        $('[data-menu-btn]').on('click', function () {
+            $('[data-header-nav]').fadeIn(300);
+            $('body').addClass('is-active');
+        });
+
+        $('[data-header-close]').on('click', function () {
+            $('[data-header-nav]').fadeOut(300);
+            $('body').removeClass('is-active');
+        });
+    }
+
+    globalMenu();
 }
