9e093c96877be8bceee81b1a00a817ca85f81b4b
hironori niho <hironori103@gmail.com>
2021-01-21 15:45:49 +0900

    ・/assets/vendor/_archives/ 配下のファイルが /dest/ に書き出されない用に修正

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('pdf', function () {
 // vendorの複製
 gulp.task('vendor', function () {
     return (
-        gulp.src(['src/assets/vendor/**/', '!src/assets/vendor/_archives/']))
+        gulp.src(['src/assets/vendor/**/', '!src/assets/vendor/_archives/', '!src/assets/vendor/_archives/**/']))
         // フォルダ以下に保存
         .pipe(gulp.dest('dest/assets/vendor/')
     );
