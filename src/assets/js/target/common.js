17ab52b7992079bdd28d816908ddfa3c3c136fca
hironori niho <hironori103@gmail.com>
2021-08-05 16:05:36 +0900

    ・ハンバーガーメニューの修正

--- src/assets/js/target/common.js
+++ src/assets/js/target/common.js
@@ -1,12 +1,27 @@
 {
     const globalMenu = () => {
+        let windowWidth = '';
+        const breakPoint = 1200;
+
+        $(window).on('load resize', function () {
+            windowWidth = window.innerWidth;
+
+            if (windowWidth >= breakPoint) {
+                $('[data-header-nav]').removeClass('is-show');
+                $('[data-header-nav]').removeClass('is-animation');
+                $('body').removeClass('is-active');
+            } else {
+                $('[data-header-nav]').addClass('is-animation');
+            }
+        });
+
         $('[data-menu-btn]').on('click', function () {
-            $('[data-header-nav]').fadeIn(300);
+            $('[data-header-nav]').addClass('is-show');
             $('body').addClass('is-active');
         });
 
         $('[data-header-close]').on('click', function () {
-            $('[data-header-nav]').fadeOut(300);
+            $('[data-header-nav]').removeClass('is-show');
             $('body').removeClass('is-active');
         });
     }
