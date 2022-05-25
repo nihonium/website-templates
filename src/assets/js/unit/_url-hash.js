f231e1fbd8eff823abb8111a3a13a930c242aa36
hironori niho <hironori103@gmail.com>
2022-05-25 13:39:14 +0900

    コードリファクタ

--- src/assets/js/unit/_url-hash.js
+++ src/assets/js/unit/_url-hash.js
@@ -1,4 +1,8 @@
 {
+    /*
+    * ページ読み込み後、アンカーリンク位置に自動遷移させる
+    * * * * * * * */
+
     const urlHash = () => {
         if (window.location.hash === "") { return; }
         document.getElementById(window.location.hash.slice(1)).scrollIntoView(true);
