ec672e05ace39af3fff37c71a87b44c511363827
hironori niho <hironori103@gmail.com>
2023-02-13 17:44:55 +0900

    コードリファクタ

--- src/assets/js/unit/_tabs-content.js
+++ src/assets/js/unit/_tabs-content.js
@@ -11,15 +11,17 @@
         function getParam(name, url) {
             if (!url) url = window.location.href;
             name = name.replace(/[\[\]]/g, "\\$&");
-            let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
+
+            const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+            const results = regex.exec(url);
+
             if (!results) return null;
             if (!results[2]) return '';
             return decodeURIComponent(results[2].replace(/\+/g, " "));
         }
 
         // ページ読み込み時のタブ切り替え
-        const tabPram = ['tab-1', 'tab-2', 'tab-3'];
+        const tabPram = ['tab-1', 'tab-2', 'tab-999'];
         const pram = getParam('p');
         if (pram && $.inArray(pram, tabPram) !== -1) {
             $('[data-tab]').removeClass('is-active');
