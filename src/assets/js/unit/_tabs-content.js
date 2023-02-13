a8aaadf0e111f3d9c905a2d5b5f6daeefdb6597c
hironori niho <hironori103@gmail.com>
2023-02-13 17:25:36 +0900

    タブ切り替え修正

--- src/assets/js/unit/_tabs-content.js
+++ src/assets/js/unit/_tabs-content.js
@@ -4,14 +4,35 @@
     * * * * * * * */
 
     const tabsContent = () => {
-        $('[data-tabs-active]').addClass('is-active');
+        // 'data-tab-active'の入ったタブを初回アクティブ表示
+        $('[data-tab-active]').addClass('is-active');
 
-        $('[data-tabs-list] .c-tabs-list__btn').click(function () {
-            const num = $('[data-tabs-list] .c-tabs-list__btn').index(this);
+        // パラメータ取得
+        function getParam(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
 
-            $(this).addClass('is-active').siblings('.c-tabs-list__btn').removeClass('is-active');
-            $(this).closest('[data-tabs-list]').next('[data-tabs-content]').find('[data-tabs-item]').removeClass('is-active');
-            $('[data-tabs-item]').eq(num).addClass('is-active');
+        // ページ読み込み時のタブ切り替え
+        const tabPram = ['tab-1', 'tab-2', 'tab-3'];
+        const pram = getParam('active-tab');
+        if (pram && $.inArray(pram, tabPram) !== -1) {
+            $('[data-tab]').removeClass('is-active');
+            $('[data-tab="' + pram + '"]').addClass('is-active');
+        }
+
+        // ロード後のタブ切り替え(クリックで対象タブを表示)
+        $('[data-tab-list] > [data-tab]').click(function () {
+            const num = $('[data-tab-list] > [data-tab]').index(this);
+
+            $(this).addClass('is-active').siblings('[data-tab]').removeClass('is-active');
+            $(this).closest('[data-tab-list]').next('[data-tab-content]').find('[data-tab-item]').removeClass('is-active');
+            $('[data-tab-item]').eq(num).addClass('is-active');
         });
     }
 
@@ -44,5 +65,5 @@
     }
 
     tabsContent();
-    tabsContentTest();
+    // tabsContentTest();
 }
