ad72baa412aa392ba933eefab6854e2338dad9f7
hironori niho <hironori103@gmail.com>
2023-02-14 14:56:44 +0900

    時間ごとの要素の出し分け

--- src/assets/js/unit/_display-time-based-elements.js
+++ src/assets/js/unit/_display-time-based-elements.js
@@ -4,29 +4,33 @@
     * * * * * * * */
 
     const displayTimeBasedElements = () => {
-        var checkTime = function () {
-            var nowTime = new Date();
+        //時・分取得する
+        let nowTime = new Date();
+        let nowHour = nowTime.getHours();
+        let nowMinute = nowTime.getMinutes();
 
-            //時・分取得する
-            var nowHour = nowTime.getHours();
-            var nowMinute = nowTime.getMinutes();
+        // 連結し数列に変換
+        let setTime = parseFloat('' + nowHour + nowMinute);
 
-            // 連結し数列に変換
-            var setTime = parseFloat('' + nowHour + nowMinute);
-
-            // 偶数奇数を判定
-            if (setTime % 2 === 0) {
-                $('#even_number').show();
-                $('#odd_number').hide();
-            } else {
-                $('#even_number').hide();
-                $('#odd_number').show();
-            }
+        // 偶数奇数を判定
+        if (setTime % 2 === 0) {
+            $('#even_number').show();
+            $('#odd_number').hide();
+        } else {
+            $('#even_number').hide();
+            $('#odd_number').show();
         }
 
-        // 1秒毎に処理を実行
-        setInterval(checkTime, 1000);
+        // 就業時間を判定
+        if ((setTime > 900) || (setTime < 1801)) {
+            $('#business_hours').show();
+            $('#off_hours').hide();
+        } else {
+            $('#business_hours').hide();
+            $('#off_hours').show();
+        }
     }
 
-    displayTimeBasedElements();
+    // 1秒毎に処理を実行
+    setInterval(displayTimeBasedElements, 1000);
 }
