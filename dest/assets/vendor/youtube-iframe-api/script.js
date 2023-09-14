be953dc4505ebf771b0be3608828f7d9e3bbe6e4
hironori niho <hironori103@gmail.com>
2023-09-14 16:24:55 +0900

    複数のモーダル表示のYouTube動画を自動再生

--- dest/assets/vendor/youtube-iframe-api/script.js
+++ dest/assets/vendor/youtube-iframe-api/script.js
@@ -5,9 +5,9 @@
 */
 
 const videoPlayButtons = document.querySelectorAll('[data-video-play]');
-const modalCloseButton = document.getElementById('modal_close');
+const modalCloseButton = document.getElementById('auto-play-close');
 const modalClose = document.getElementById('auto-play-modal');
-const modal = document.getElementById('modal');
+const modal = document.getElementById('auto-play');
 const videoIframes = document.querySelectorAll('[data-youtube-video]');
 let currentVideoId = null;
 
