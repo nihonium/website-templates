c3511f38a17cd83cf8d4014929dc6196cd2cf370
hironori niho <hironori103@gmail.com>
2023-10-26 10:45:08 +0900

    入力フォームの修正

--- src/assets/js/unit/_handle-submit.js
+++ src/assets/js/unit/_handle-submit.js
@@ -1,13 +1,12 @@
 {
-    const formElement = document.querySelector('form');
-
-    formElement.addEventListener('submit', handleSubmit);
-
-    function handleSubmit(event) {
+    const formElement = document.getElementById('postForm');
+    const handleSubmit = (event) => {
         const isYes = confirm('この内容で送信していいですか？');
 
         if (isYes === false) {
             event.preventDefault();
         }
-    }
+    };
+
+    formElement.addEventListener('submit', handleSubmit);
 }
