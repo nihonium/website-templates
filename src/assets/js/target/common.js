e882bcadc1a7cb635310c40f1612c78f5c2ff3a8
hironori niho <hironori103@gmail.com>
2023-11-29 11:31:36 +0900

    ドロップダウンメニューの作成

--- src/assets/js/target/common.js
+++ src/assets/js/target/common.js
@@ -1,16 +1,13 @@
 /** グローバルメニュー */
 {
     const globalMenu = () => {
-
         /**
          * 画面幅を格納
-         * @type {number}
          */
         let windowWidth = '';
 
         /**
          * ブレイクポイントを指定
-         * @type {number}
          */
         const breakPoint = 1200;
 
@@ -37,17 +34,69 @@
         });
     }
 
+    const globalDropDown = () => {
+        const main = document.querySelectorAll('[data-dropdown]');
+        const item = Array.prototype.slice.call(main, 0);
+
+        /**
+         * 画面幅を格納
+         */
+        let windowWidth = '';
+
+        /**
+         * ブレイクポイントを指定
+         */
+        const breakPoint = 1200;
+
+        $(window).on('load resize', function () {
+            windowWidth = window.innerWidth;
+
+            item.forEach(function (ele) {
+                ele.querySelector('[data-dropdown-list]').removeAttribute('style');
+                ele.querySelector('[data-dropdown-btn]').classList.remove('is-open');
+                ele.querySelector('[data-dropdown-list]').classList.remove('is-open');
+            });
+
+            if (windowWidth >= breakPoint) {
+                item.forEach(function (ele) {
+                    ele.addEventListener('mouseover', function () {
+                        ele.querySelector('[data-dropdown-list]').classList.add('is-open');
+                    }, false);
+                    ele.addEventListener('mouseout', function () {
+                        ele.querySelector('[data-dropdown-list]').classList.remove('is-open');
+                    }, false);
+                });
+            } else {
+                $('[data-dropdown-btn]').on('click', function () {
+                    $(this).toggleClass('is-open').next().slideToggle();
+                });
+            }
+        });
+    }
+
     globalMenu();
+    globalDropDown();
 
-    const main = document.querySelectorAll('[data-menu-dropdown]');
-    const item = Array.prototype.slice.call(main,0);
+    const mediaQueryList = window.matchMedia('(min-width: 768px)');
 
-    item.forEach(function (element) {
-    element.addEventListener("mouseover",function(){
-        element.querySelector('[data-menu-dropdown-list]').classList.add('open');
-    },false);
-    element.addEventListener("mouseout",function(){
-        element.querySelector('[data-menu-dropdown-list]').classList.remove('open');
-    },false);
-    });
+    /**
+     * イベントリスナー
+     */
+    const listener = (event) => {
+    // リサイズ時に行う処理
+    if (event.matches) {
+        // 768px以上
+        console.log('PC用ブレークポイント用処理');
+    } else {
+        // 768px未満
+        console.log('SP用ブレークポイント用処理');
+    }
+    };
+
+    // リスナー登録
+    // mediaQueryList.addListener(listener); // @deprecated
+    mediaQueryList.addEventListener("change", listener);
+
+    // 初期化処理
+    listener(mediaQueryList);
 }
