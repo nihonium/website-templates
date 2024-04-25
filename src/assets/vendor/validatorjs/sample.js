5e7a0748bf139924d90f916cab26f7cd9d77a64a
hironori niho <hironori103@gmail.com>
2024-04-25 12:27:40 +0900

    validatorjsを使ったバリデーション実装

--- src/assets/vendor/validatorjs/sample.js
+++ src/assets/vendor/validatorjs/sample.js
@@ -4,35 +4,27 @@
  * https://qiita.com/y-miine/items/615d4fc29327c64d661f
  * https://github.com/mikeerickson/validatorjs
  */
-
 function validateForm() {
     Validator.useLang('ja');
 
     // カスタムルール：年齢
     Validator.register('age_check', function(value, requirement, attribute) {
         return value >= requirement;
-    },
-        '年齢は18歳以上である必要があります。'
-    );
+    }, '年齢は18歳以上である必要があります。');
 
     // カスタムルール：メールアドレスの確認
     Validator.register('email_confirmation', function(value, requirement, attribute) {
         const confirmationField = attribute.replace('_confirmation', '');
         const confirmationValue = document.getElementById(confirmationField).value;
         return value === confirmationValue;
-    },
-        'メールアドレスが一致しません。'
-    );
+    }, 'メールアドレスが一致しません。');
 
     // カスタムルール：ラジオボタンの必須入力検証
-    Validator.register('required_radio', function(value, requirement, attribute) {
-        let radioButtons = document.querySelectorAll('input[name="' + attribute + '"]');
-        for (let i = 0; i < radioButtons.length; i++) {
-            if (radioButtons[i].checked) {
-                return true;
-            }
+    Validator.register('required_radio', function (value, requirement, attribute) {
+        const radios = $('input[name="' + attribute + '"]').length;
+        if (radios === 0) {
+            return true;
         }
-        return false;
     }, ':attributeは必須項目です。');
 
     // カスタムルール：チェックボックスの必須入力検証
@@ -40,22 +32,21 @@ function validateForm() {
         return document.getElementById(attribute).checked;
     }, ':attributeは必須項目です。');
 
-
     let data = {
         name: document.getElementById('name').value,
-        age: document.getElementById('age').value,
-        gender: getSelectedRadioValue('gender'),
         email: document.getElementById('email').value,
         email_confirmation: document.getElementById('email_confirmation').value,
+        age: document.getElementById('age').value,
+        gender: getSelectedRadioValue('gender'),
         accept_terms: document.getElementById('accept_terms').checked
     };
 
     let rules = {
         name: 'required',
-        age: 'required|age_check:18',
-        gender: 'required_radio',
         email: 'required|email',
         email_confirmation: 'required|email_confirmation',
+        age: 'required|age_check:18',
+        gender: 'required_radio',
         accept_terms: 'required_checkbox'
     };
 
@@ -64,10 +55,10 @@ function validateForm() {
     // フィールド名を日本語に変換
     validation.setAttributeNames({
         name: '名前',
-        age: '年齢',
-        gender: '性別',
         email: 'メールアドレス',
         email_confirmation: '確認用メールアドレス',
+        age: '年齢',
+        gender: '性別',
         accept_terms: '注意事項・利用規約への同意'
     });
 
@@ -75,11 +66,6 @@ function validateForm() {
     let messages = Validator.getMessages('ja');
     messages.required = '【:attribute】は必須項目です。';
     Validator.setMessages('ja', messages);
-
-    // ラジオボタンが選択されていない場合に手動でエラーメッセージを追加
-    if (!document.querySelector('input[name="gender"]:checked')) {	
-        validation.errors.add('gender', '【性別】は必須項目です。');	
-    }
 
     if (validation.passes()) {
         alert('フォームが正常に送信されました！');
@@ -106,5 +92,5 @@ function getSelectedRadioValue(name) {
             return radios[i].value;
         }
     }
-    return null;
+    return null; // ラジオボタンが選択されていない場合はnullを返す
 }
