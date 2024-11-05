b6b966a7508ef718bd8bffc2dfa42889d0f0cd53
hironori niho <hironori103@gmail.com>
2024-11-05 12:05:09 +0900

    コードリファクタ

--- src/assets/js/unit/_svg_animation.js
+++ src/assets/js/unit/_svg_animation.js
@@ -1,83 +1,32 @@
-gsap.registerPlugin(ScrollTrigger);
+const gsapSectionLine = () => {
+    /**
+     * GASP プラグイン指定
+     */
+    gsap.registerPlugin(ScrollTrigger);
 
-const path1 = document.querySelector("#path1");
-const pathLength1 = path1.getTotalLength();
+    const paths = document.querySelectorAll("[data-parallax-line]");
 
-const path2 = document.querySelector("#path2");
-const pathLength2 = path2.getTotalLength();
+    paths.forEach(path => {
+        const pathLength = path.getTotalLength();
 
-const path3 = document.querySelector("#path3");
-const pathLength3= path3.getTotalLength();
+        // 書き出しを逆にする場合は`strokeDashoffset: -pathLength`に変更する
+        gsap.set(path, {
+            strokeDasharray: pathLength,
+            strokeDashoffset: pathLength
+        });
 
-const path4 = document.querySelector("#path4");
-const pathLength4 = path4.getTotalLength();
+        gsap.to(path, {
+            strokeDashoffset: 0,
+            duration: 2, // アニメーションの時間（秒）
+            ease: "power4.inOut", // アニメーションの緩急
+            scrollTrigger: {
+                trigger: path,
+                start: "top 80%", // ビューの80%に来た時に開始
+                end: "bottom 20%", // ビューの20%までに完了（無視されるが定義）
+                toggleActions: "play none none none" // アニメーションの制御
+            }
+        });
+    });
+}
 
-// 初期状態: 線を全て隠す
-gsap.set(path1, {
-    strokeDasharray: pathLength1,
-    strokeDashoffset: pathLength1
-});
-
-gsap.set(path2, {
-    strokeDasharray: pathLength2,
-    strokeDashoffset: pathLength2
-});
-
-gsap.set(path3, {
-    strokeDasharray: pathLength3,
-    strokeDashoffset: pathLength3
-});
-
-gsap.set(path4, {
-    strokeDasharray: pathLength4,
-    strokeDashoffset: pathLength4
-});
-
-// スクロールトリガーで線を描くアニメーション
-gsap.to(path1, {
-    strokeDashoffset: 0,
-    duration: 1.5,// アニメーションの時間（秒）
-    ease: "power1.inOut", // アニメーションの緩急
-    scrollTrigger: {
-        trigger: path1,
-        start: "top 80%", // ビューの80%に来た時に開始
-        end: "bottom 20%", // ビューの20%までに完了（無視されるが定義）
-        toggleActions: "play none none none", // アニメーションの制御
-    }
-});
-
-gsap.to(path2, {
-    strokeDashoffset: 0,
-    duration: 1.5,// アニメーションの時間（秒）
-    ease: "power2.inOut", // アニメーションの緩急
-    scrollTrigger: {
-        trigger: path2,
-        start: "top 80%", // ビューの80%に来た時に開始
-        end: "bottom 20%", // ビューの20%までに完了（無視されるが定義）
-        toggleActions: "play none none none", // アニメーションの制御
-    }
-});
-
-gsap.to(path3, {
-    strokeDashoffset: 0,
-    duration: 1.5,// アニメーションの時間（秒）
-    ease: "power3.inOut", // アニメーションの緩急
-    scrollTrigger: {
-        trigger: path3,
-        start: "top 80%", // ビューの80%に来た時に開始
-        end: "bottom 20%", // ビューの20%までに完了（無視されるが定義）
-        toggleActions: "play none none none", // アニメーションの制御
-    }
-});
-
-gsap.to(path4, {
-    strokeDashoffset: 0,
-    duration: 1.5,// アニメーションの時間（秒）
-    ease: "power4.inOut", // アニメーションの緩急
-    scrollTrigger: {
-        trigger: path4,
-        start: "top 80%", // ビューの80%に来た時に開始
-        end: "bottom 20%", // ビューの20%までに完了（無視されるが定義）
-        toggleActions: "play none none none", // アニメーションの制御
-    }
-});
+document.addEventListener('DOMContentLoaded', gsapSectionLine);
