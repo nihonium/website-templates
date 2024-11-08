657f59c75cf055240378038445fbb35040e0158e
hironori niho <hironori103@gmail.com>
2024-11-08 18:24:33 +0900

    動画埋め込みテスト

--- src/assets/js/target/test-video.js
+++ src/assets/js/target/test-video.js
@@ -6,46 +6,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     var kv = document.getElementById('kv');
     var kvLoop = document.getElementById('kv-loop');
-    var kvState = document.getElementById('kv-state');
-    var kvLoopState = document.getElementById('kv-loop-state');
 
-    //ロード開始
-    kv.addEventListener('loadedmetadata', function () {
-        kvState.textContent = '1つ目の動画 ロードを開始しました';
-    })
-    //読み込み完了
-    kv.addEventListener('loadeddata', function () {
-        kvState.textContent = '1つ目の動画 読み込み完了しました';
-    })
-    //再生可能
-    kv.addEventListener('canplay', function () {
-        kvState.textContent = '1つ目の動画 再生可能です';
-    })
-    //再生中
-    kv.addEventListener('playing', function () {
-        kvState.textContent = '1つ目の動画 再生中です';
-    })
-    //再生終了
+    // 最初の動画が再生終了したら2つ目の動画を再生
     kv.addEventListener('ended', function () {
         kv.parentNode.classList.add("is-hide");
-        kvLoop.muted = true;
-        kvLoop.loop = true;
         kvLoop.play();
-    })
-    //ロード開始
-    kvLoop.addEventListener('loadedmetadata', function () {
-        kvLoopState.textContent = '2つ目の動画 ロードを開始しました';
-    })
-    //読み込み完了
-    kvLoop.addEventListener('loadeddata', function () {
-        kvLoopState.textContent = '2つ目の動画 読み込み完了しました';
-    })
-    //再生可能
-    kvLoop.addEventListener('canplay', function () {
-        kvLoopState.textContent = '2つ目の動画 再生可能です';
-    })
-    //再生中
-    kvLoop.addEventListener('playing', function () {
-        kvLoopState.textContent = '2つ目の動画 再生中です';
     })
 });
